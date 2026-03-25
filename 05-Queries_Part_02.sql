dabee443f0ee1afe4ee39ece9cba8983c170bc05
firoz.nur <firoznur5@gmail.com>
2026-03-25 11:43:32 +0600

    Add : Sub Queries

--- 05-Queries_Part_02.sql
+++ 05-Queries_Part_02.sql
@@ -36,3 +36,61 @@ FROM
 
 ALTER TABLE publishers
 DROP COLUMN publisher_id;
+
+SELECT
+  *
+FROM
+  publishers
+WHERE
+  id IN (
+    SELECT
+      publisher_id
+    FROM
+      books
+    GROUP BY
+      publisher_id
+    HAVING
+      COUNT(*) > 1
+  );
+
+SELECT
+  *
+FROM
+  publishers p
+WHERE
+  EXISTS (
+    SELECT
+      *
+    FROM
+      books b
+    WHERE
+      b.publisher_id = p.id
+      AND b.in_stock = TRUE
+  );
+
+SELECT
+  *
+FROM
+  books
+WHERE
+  publisher_id IN (
+    SELECT
+      id
+    FROM
+      publishers
+    WHERE
+      name ILIKE '%penguin%'
+  );
+
+SELECT
+  *
+FROM
+  books b
+  JOIN publishers p ON b.publisher_id = p.id
+WHERE
+  name ILIKE '%penguin%';
+
+SELECT
+  *
+FROM
+  books;
